14b88222d1cb3efb1effb3455d829e5fa8e16e8a
Brian Mann <brian@osysit.com>
2013-04-12 23:36:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimalRescueHub::Application.config.secret_token = '861da4889c3e00811f5dc23724a096606f4ccbd8a5cd87b3dd0b92a84484bf7e6446c6e9c946fe7f868e893815ccf5aa88f1649d1a7220406de96a0fff59ea7c'
