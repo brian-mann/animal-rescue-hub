1d3dfe0046aa715884cc17cc8f007ce544eef4f5
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-04-30 11:48:57 -0400

    Updated User table to include password_digest.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130413190850) do
+ActiveRecord::Schema.define(:version => 20130430154219) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -111,5 +111,16 @@ ActiveRecord::Schema.define(:version => 20130413190850) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
