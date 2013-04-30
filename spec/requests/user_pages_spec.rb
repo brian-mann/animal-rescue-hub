695f0d773c52ef2b5cb063bf19e1fded2cd894f3
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-04-30 14:36:08 -0400

    Added failing test for Signup Form.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,4 +12,30 @@ describe "User pages" do
 	  it          { should have_selector('title',  text: user.first_name) }
 	end
 
+	describe "Sign Up page" do
+
+		before { visit signup_path }
+
+		let(:submit) { "Create my Account" }
+
+		describe "with invalid information" do
+			it "should not create a user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
+
+		describe "with valid information" do
+			before do
+				fill_in "First Name",						with: "Example"
+				fill_in "Last Name",						with: "User"
+				fill_in "Email",								with: "user@example.com"
+				fill_in "Password",							with: "foobar"
+				fill_in "PW Confirmation",			with: "foobar"
+			end
+
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
+	end
 end
