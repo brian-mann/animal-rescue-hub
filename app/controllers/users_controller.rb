3e3ec90e60b8629bb0fec4cbb19485b0d9a4a90c
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-05-03 12:15:35 -0400

    Added title to "new user" page. Added logic to create new user then show user profile after create.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,18 @@ class UsersController < ApplicationController
 	end
 
 	def new
+		@title = "Sign Up"
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Welcome to Rescue Funder!"
+			redirect_to @user
+		else
+			render 'new'
+		end
 	end
 
 end
