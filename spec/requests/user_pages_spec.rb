237e223f9c273a5ffe33544b30d65dce7ba11518
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-05-06 13:52:15 -0400

    wrote test to check that a new user is signed in.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -54,6 +54,7 @@ describe "User pages" do
 
 			  it { should have_selector('title', text: user.full_name) }
 			  it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			  it { should have_link('Sign Out') }
 			end
 
 		end
