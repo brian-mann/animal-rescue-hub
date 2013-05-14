366772cd3db2b88421967d27a7d7535ba5773014
Jennifer Shehane <shehane.jennifer@gmail.com>
2013-05-14 12:18:30 -0400

    Wrote logic for admin_users (only allow them to access user's index).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_filter :signed_in_user, only: [:index, :edit, :update]
-	before_filter :correct_user,   only: [:edit, :update]
+	before_filter :require_admin_user, 			only: [:index]
+	before_filter :signed_in_user, 					only: [:edit, :update]
+	before_filter :correct_user,   					only: [:edit, :update]
 
 	def index
 		@users = User.paginate(page: params[:page], :per_page => 50)
